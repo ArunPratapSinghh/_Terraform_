1892abca755ebfc9296e4719ec795a910030f5f6
ArunPratapSingh <180103642+ArunPratapSinghh@users.noreply.github.com>
2025-03-19 17:36:11 +0530

    Update main.docker.tf

--- main.docker.tf
+++ main.docker.tf
@@ -1,1 +1,13 @@
 ####Creating docker file in terraform######
+
+terraform{
+    required_provider{
+docker={
+    
+    version = "kreuzworker/docker"
+    source = "~>2.21.0"
+      }
+  }
+}
+
+provider "docker" {}
