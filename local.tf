b99ea6d412bfcdfc8491139b396eab1ac3d0e44d
ArunPratapSingh <180103642+ArunPratapSinghh@users.noreply.github.com>
2025-03-19 11:43:25 +0530

    Update local.tf

--- local.tf
+++ local.tf
@@ -7,3 +7,10 @@ resource "local_file" "Devops"{
       content = "I am learning the devops for my growith in IT industry."
 
 }
+
+resource "local_txt" "Server_Details" {
+
+       filename = "/home/aps/desktop/Server_test.txt"
+       content = "I know the server details"
+
+}
